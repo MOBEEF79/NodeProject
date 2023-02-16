bf23d38e891497acc065b174582e21d89eeef966
MOBEEF79 <meauldflipflop@yahoo.com>
2023-02-16 15:17:04 +0000

    Changed welcome message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Howdelly Ho Neighbourino!'))
 
 app.listen(5000, () => console.log(`App running on port ${port}`));
